26225b68ca50bee9efb71f0df318541da7675ff9
Ialberquilla <ivan.alberquilla@gmail.com>
2023-04-24 19:27:13 +0200

    group list plugin

--- contracts/contracts/plugins/GroupVotingPlugin/GroupVotingSetup.sol
+++ contracts/contracts/plugins/GroupVotingPlugin/GroupVotingSetup.sol
@@ -7,18 +7,18 @@ import {DAO} from "@aragon/osx/core/dao/DAO.sol";
 import {PermissionLib} from "@aragon/osx/core/permission/PermissionLib.sol";
 import {PluginSetup, IPluginSetup} from "@aragon/osx/framework/plugin/setup/PluginSetup.sol";
 import {MajorityVotingBase} from "@aragon/osx/plugins/governance/majority-voting/MajorityVotingBase.sol";
-import {AddresslistVoting} from "./GroupVoting.sol";
+import {GroupVoting} from "./GroupVoting.sol";
 
 /// @title AddresslistVotingSetup
 /// @author Aragon Association - 2022-2023
 /// @notice The setup contract of the `AddresslistVoting` plugin.
 contract AddresslistVotingSetup is PluginSetup {
     /// @notice The address of `AddresslistVoting` plugin logic contract to be used in creating proxy contracts.
-    AddresslistVoting private immutable addresslistVotingBase;
+    GroupVoting private immutable groupVotingBase;
 
     /// @notice The contract constructor, that deployes the `AddresslistVoting` plugin logic contract.
     constructor() {
-        addresslistVotingBase = new AddresslistVoting();
+        groupVotingBase = new GroupVoting();
     }
 
     /// @inheritdoc IPluginSetup
@@ -32,9 +32,9 @@ contract AddresslistVotingSetup is PluginSetup {
 
         // Prepare and Deploy the plugin proxy.
         plugin = createERC1967Proxy(
-            address(addresslistVotingBase),
+            address(groupVotingBase),
             abi.encodeWithSelector(
-                AddresslistVoting.initialize.selector,
+                GroupVoting.initialize.selector,
                 _dao,
                 votingSettings,
                 members
@@ -52,7 +52,7 @@ contract AddresslistVotingSetup is PluginSetup {
             plugin,
             _dao,
             PermissionLib.NO_CONDITION,
-            addresslistVotingBase.UPDATE_ADDRESSES_PERMISSION_ID()
+            groupVotingBase.UPDATE_ADDRESSES_PERMISSION_ID()
         );
 
         permissions[1] = PermissionLib.MultiTargetPermission(
@@ -60,7 +60,7 @@ contract AddresslistVotingSetup is PluginSetup {
             plugin,
             _dao,
             PermissionLib.NO_CONDITION,
-            addresslistVotingBase.UPDATE_VOTING_SETTINGS_PERMISSION_ID()
+            groupVotingBase.UPDATE_VOTING_SETTINGS_PERMISSION_ID()
         );
 
         permissions[2] = PermissionLib.MultiTargetPermission(
@@ -68,7 +68,7 @@ contract AddresslistVotingSetup is PluginSetup {
             plugin,
             _dao,
             PermissionLib.NO_CONDITION,
-            addresslistVotingBase.UPGRADE_PLUGIN_PERMISSION_ID()
+            groupVotingBase.UPGRADE_PLUGIN_PERMISSION_ID()
         );
 
         // Grant `EXECUTE_PERMISSION` of the DAO to the plugin.
@@ -97,7 +97,7 @@ contract AddresslistVotingSetup is PluginSetup {
             _payload.plugin,
             _dao,
             PermissionLib.NO_CONDITION,
-            addresslistVotingBase.UPDATE_ADDRESSES_PERMISSION_ID()
+            groupVotingBase.UPDATE_ADDRESSES_PERMISSION_ID()
         );
 
         permissions[1] = PermissionLib.MultiTargetPermission(
@@ -105,7 +105,7 @@ contract AddresslistVotingSetup is PluginSetup {
             _payload.plugin,
             _dao,
             PermissionLib.NO_CONDITION,
-            addresslistVotingBase.UPDATE_VOTING_SETTINGS_PERMISSION_ID()
+            groupVotingBase.UPDATE_VOTING_SETTINGS_PERMISSION_ID()
         );
 
         permissions[2] = PermissionLib.MultiTargetPermission(
@@ -113,7 +113,7 @@ contract AddresslistVotingSetup is PluginSetup {
             _payload.plugin,
             _dao,
             PermissionLib.NO_CONDITION,
-            addresslistVotingBase.UPGRADE_PLUGIN_PERMISSION_ID()
+            groupVotingBase.UPGRADE_PLUGIN_PERMISSION_ID()
         );
 
         permissions[3] = PermissionLib.MultiTargetPermission(
@@ -127,6 +127,6 @@ contract AddresslistVotingSetup is PluginSetup {
 
     /// @inheritdoc IPluginSetup
     function implementation() external view returns (address) {
-        return address(addresslistVotingBase);
+        return address(groupVotingBase);
     }
 }
